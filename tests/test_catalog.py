1c4fd502ff2ababa8dca4b558f9adb9b5e4a6ff4
realvolgin1-beep <realvolgin1@gmail.com>
2026-08-29 19:22:28 +1000

    Расширить бесплатный нормативный поиск: tests/test_catalog.py

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -15,3 +15,25 @@ def test_road_sign_query_finds_traffic_standard():
     hits = search_catalog("установка дорожных знаков")
 
     assert hits[0]["code"] == "ГОСТ Р 52289-2019"
+
+
+def test_typo_in_bridge_query_is_understood():
+    hits = search_catalog("пролтное строние")
+
+    assert hits[0]["code"] == "СП 35.13330.2011"
+
+
+def test_fire_query_finds_evacuation_rules():
+    hits = search_catalog("ширина эвакуационного выхода")
+
+    assert hits[0]["code"] == "СП 1.13130.2020"
+
+
+def test_masonry_query_finds_masonry_rules():
+    hits = search_catalog("кирпичная кладка")
+
+    assert hits[0]["code"] == "СП 15.13330.2020"
+
+
+def test_unknown_document_number_is_not_invented():
+    assert search_catalog("СП46") == []
